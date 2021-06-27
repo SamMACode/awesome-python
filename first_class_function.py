4a81ee909c74807ea7ddb52376def47a35fc1884
sam ma <sam_newyork@163.com>
2021-06-27 18:16:09 +0800

    get fuction parameter through signature from inspect module

--- first_class_function.py
+++ first_class_function.py
@@ -1,3 +1,9 @@
+from functools import reduce
+from operator import add
+import random
+from inspect import signature
+
+
 def factorial(n):
     """returns n!"""
     return 1 if n < 2 else n * factorial(n - 1)
@@ -35,4 +41,79 @@ lambdaFilter = list(map(factorial, filter(lambda n: n % 2, range(6))))
 arrayFilter = [factorial(n) for n in range(6) if n % 2]
 # lambdaFilter: [1, 6, 120], array filter: [1, 6, 120]
 print(f"lambdaFilter: {lambdaFilter}, array filter: {arrayFilter}")
+
+# use reduce and sum function to calculate data
+# reduce(add, range(100)): 4950, sum(range(100)): 4950
+print(f"reduce(add, range(100)): {reduce(add, range(100))}, sum(range(100)): {sum(range(100))}")
+
+
+class BingoCage:
+    def __init__(self, items):
+        self._items = list(items)
+        random.shuffle(self._items)
+
+    def pick(self):
+        try:
+            return self._items.pop()
+        except IndexError:
+            raise LookupError('pick from empty BingoCage')
+
+    def __call__(self):
+        return self.pick()
+
+
+bingo = BingoCage(range(3))
+# bingo.pick(): 1, bingo(): 2, callable(bingo): True
+print(f"bingo.pick(): {bingo.pick()}, bingo(): {bingo()}, callable(bingo): {callable(bingo)}")
+
+
+# define class C and UDF func()
+class C:
+    pass
+
+
+def func(): pass
+
+
+obj = C()
+# (dir(func) - dir(func) methods: ['__annotations__', '__call__', '__closure__', '__code__',
+# '__defaults__', '__get__', '__globals__', '__kwdefaults__', '__name__', '__qualname__']
+diff = sorted(set(dir(func)) - set(dir(obj)))
+print(f"(dir(func) - dir(func) methods: {diff}")
+
+
+def tag(name, *content, cls=None, **attrs):
+    """generate one or more tags"""
+    if cls is not None:
+        attrs['class'] = cls
+    if attrs:
+        attr_str = ''.join(' %s="%s"' % (attr, value)
+                           for attr, value in
+                           sorted(attrs.items()))
+    else:
+        attr_str = ''
+    if content:
+        return '\n'.join('<%s%s>%s</%s>' %
+                         (name, attr_str, c, name) for c in content)
+    else:
+        return '<%s%s />' % (name, attr_str)
+
+
+# tag('br'): <br />, tag('p', 'hello'): <p>hello</p>, <p id="33">hello</p>
+print(f"tag('br'): {tag('br')}, tag('p', 'hello'): {tag('p', 'hello')}, tag('p', 'hello', id=33): "
+      f"{tag('p', 'hello', id=33)}")
+# extract the function signature, such as tag function
+sig = signature(tag)
+# (name, *content, cls=None, **attrs), KEYWORD_ONLY : cls = None
+print(f"signature of tag function: {sig}")
+for name, param in sig.parameters.items():
+    print(param.kind, ":", name, "=", param.default)
+
+my_tag = {'name': 'img', 'title': 'Sunset Boulevard', 'src': 'sunset.jpg', 'cls': 'framed'}
+bound_args = sig.bind(**my_tag)
+for name, value in bound_args.arguments.items():
+    print(name, '=', value)
+del my_tag['name']
+# TypeError: missing a required argument: 'name'
+# bound_args = sig.bind(**my_tag)
 
