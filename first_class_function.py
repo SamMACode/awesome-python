5ab3fef857b9a1fcf0d2b9106b46ac307ec43e4c
sam ma <sam_newyork@163.com>
2021-07-03 20:33:01 +0800

    use attrgetter and methodcaller to extract object filed value

--- first_class_function.py
+++ first_class_function.py
@@ -2,6 +2,8 @@ from functools import reduce
 from operator import add
 import random
 from inspect import signature
+from operator import itemgetter
+from collections import namedtuple
 
 
 def factorial(n):
@@ -117,3 +119,39 @@ del my_tag['name']
 # TypeError: missing a required argument: 'name'
 # bound_args = sig.bind(**my_tag)
 
+
+# factorial implemented with reduce and anonymous function
+def fun(n):
+    return reduce(lambda a, b: a*b, range(1, n+1))
+
+
+metro_data = [('Tokyo', 'JP', 36.933, (35.689722, 139.691667)),
+              ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889)),
+              ('New York-Newark', 'US', 20.104, (40.808611, -74.020386)),
+              ('Mexico City', 'MX', 20.142, (19.433333, -99.133333)),
+              ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833))]
+# ('Sao Paulo', 'BR', 19.649, (-23.547778, -46.635833))
+# ('Delhi NCR', 'IN', 21.935, (28.613889, 77.208889))
+for city in sorted(metro_data, key=itemgetter(1)):
+    print(city)
+
+LatLong = namedtuple('LatLong', 'lat long')
+Metropolis = namedtuple('Metropolis', 'name cc pop coord')
+metro_areas = [Metropolis(name, cc, pop, LatLong(lat, long))
+               for name, cc, pop, (lat, long) in metro_data]
+# metro_areas[0].coord.lat: 35.689722
+print(f"metro_areas[0]: {metro_areas[0]}, metro_areas[0].coord.lat: {metro_areas[0].coord.lat}")
+
+from operator import attrgetter
+name_lat = attrgetter('name', 'coord.lat')
+# ('Sao Paulo', -23.547778)
+# ('Mexico City', 19.433333)
+for city in sorted(metro_areas, key=attrgetter('coord.lat')):
+    print(name_lat(city))
+
+from operator import methodcaller
+s = 'The time has come'
+upcase = methodcaller('upper')
+hiphenate = methodcaller('replace', ' ', '-')
+# upcase(s): THE TIME HAS COME, hiphenate(s): The-time-has-come
+print(f"upcase(s): {upcase(s)}, hiphenate(s): {hiphenate(s)}")
