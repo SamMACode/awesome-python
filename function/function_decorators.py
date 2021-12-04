fcdeb91d7f92b648cb43942ad6a237d68d06a588
sam ma <sam_newyork@163.com>
2021-12-04 21:49:23 +0800

    define custom exception to catch program error

--- function/function_decorators.py
+++ function/function_decorators.py
@@ -12,6 +12,15 @@ class Averager():
         return total/len(self.series)
 
 
+class MyInputError(Exception):
+    """Exception raised when there're errors in input"""
+    def __init__(self, value): # 自定义异常信息的初始化
+        self.value = value
+
+    def __str__(self): # 自定义异常类型的string表达式
+        return "{} is invalid input".format(repr(self.value))
+
+
 if __name__ == '__main__':
     avg = Averager()
     print(f"avg(10): {avg(10)}, avg(11): {avg(11)}, avg(12): {avg(12)}")
@@ -19,3 +28,9 @@ if __name__ == '__main__':
     data = functools.reduce(lambda a, b: a*b, range(1, 6))
     # data: 120
     print(f"data: {data}")
+
+    try:
+        # error: 1 is invalid input
+        raise MyInputError(1)
+    except MyInputError as err:
+        print('error: {}'.format(err))
