bd413be16a6a6a5e8d842a4b872a6572078b0cb5
sam ma <sam_newyork@163.com>
2021-12-30 00:51:30 +0800

    use bs4 and async、await strategy to fetch data concurrency

--- concurrency/coroutine.py
+++ concurrency/coroutine.py
@@ -1,6 +1,8 @@
 # -*- coding:utf-8 -*-
 import time
 import asyncio
+import aiohttp
+from bs4 import BeautifulSoup
 
 
 async def crawl_page(url):
@@ -23,5 +25,42 @@ async def metrics():
 	print(f"total used {round(time.time() - start_time, 2)} s for crawling webpage")
 
 
+async def fetch_content(url):
+	"""通过url从豆瓣网来抓取数据，async、await来等待response.text()结束"""
+	async with aiohttp.ClientSession(
+			headers={'users-agent': 'Mozilla/5.0'}, connector=aiohttp.TCPConnector(ssl=False)
+	) as session:
+		async with session.get(url) as response:
+			return await response.text()
+
+
+async def use_bs4_fetch_data():
+	"""通过beautiful_Soup4解析python爬到的数据，从doc中解析缩略图"""
+	url = "https://movie.douban.com/cinema/later/beijing/"
+	init_page = await fetch_content(url)
+	init_soup = BeautifulSoup(init_page, 'lxml')
+
+	movie_names, urls_to_fetch, movie_dates = [], [], []
+	all_movies = init_soup.find('div', id='showing-soon')
+	for each_movie in all_movies.find_all('div', class_="item"):
+		all_a_tag = each_movie.find_all('a')
+		all_li_tag = each_movie.find_all('li')
+
+		# 从<a>链接中提取href、date日期、tag#text中影片名称，并通过printf打印console
+		movie_names.append(all_a_tag[1].text)
+		urls_to_fetch.append(all_a_tag[1]['href'])
+		movie_dates.append(all_li_tag[0].text)
+	tasks = [fetch_content(url) for url in urls_to_fetch]
+	pages = await asyncio.gather(*tasks)
+
+	for movie_name, movie_date, page in zip(movie_names, movie_dates, pages):
+		soup_item = BeautifulSoup(page, 'lxml')
+		img_tag = soup_item.find('img')
+	# 穿过寒冬拥抱你 12月31日 https://img1.doubanio.com/view/photo/s_ratio_poster/public/p2801718909.jpg
+	# 以年为单位的恋爱 12月31日 https://img9.doubanio.com/view/photo/s_ratio_poster/public/p2715903096.jpg
+		print('{} {} {}'.format(movie_name, movie_date, img_tag['src']))
+
+
 if __name__ == '__main__':
-	asyncio.run(metrics())
+	# asyncio.run(metrics())
+	asyncio.run(use_bs4_fetch_data())
